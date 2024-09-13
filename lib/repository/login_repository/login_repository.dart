9e42866a93b9361e4cf7e75c13068477720c9ca6
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-09-13 20:26:31 +0600

    add AppUrl class

--- lib/repository/login_repository/login_repository.dart
+++ lib/repository/login_repository/login_repository.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:getx_practise/data/network/network_api_services.dart';
+import 'package:getx_practise/res/app_url/app_url.dart';
 
 class LoginRepository {
 
@@ -8,7 +9,7 @@ class LoginRepository {
 
   Future<dynamic> loginApi(var data) async {
 
-    dynamic response = _apiService.postApi(data, url);
+    dynamic response = _apiService.postApi(data, AppUrl.loginApi);
     return response;
   }
 
